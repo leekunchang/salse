45b619ea1f96b80e55f9066af67ba56ef72477b4
KUNCHANG LEE <lkc9233@naver.com>
2022-01-28 08:59:42 +0900

    대표님 실행파일추가

--- AutoList_DAPYONIM.py
+++ AutoList_DAPYONIM.py
@@ -5,7 +5,8 @@ import pyautogui as pag
 # pos = pag.position()
 # print(pos)
 
-# model_cord = ["CN", "DN", "IG", "QX", "OS", "NX", "TM", "LX", "US", "M5"]
+model_cord = "CN"   # ---------------------조회 희망 차량코드 입력하세요
+playtime = 2 # ----------------------------조회 희망 횟수입니다
 
 pag.click(711,1057) #국판 화면 클릭
 time.sleep(1) # 화면띄우는 시간 1초지연
@@ -14,7 +15,7 @@ pag.hotkey('ctrl','a') # 전체 복사
 pag.press('delete')
 time.sleep(0.5) # 화면띄우는 시간 0.5초지연
 pag.click(487,280) # 사양입력창 재클릭
-pag.typewrite(model_cord.index(0)) # CN(희망차량코드) 입력
+pag.typewrite(model_cord) # CN(희망차량코드) 입력
 pag.press('enter') # 조회
 pag.press('tap') # 탭 입력
 pag.press('end') # 스크롤 최하단 이동
@@ -22,7 +23,7 @@ pag.press('pgdn') # 조회
 time.sleep(5) # 화면띄우는 시간 5초지연
 
 salse_list = 0
-while salse_list < 5:
+while salse_list < playtime:
     salse_list = salse_list + 1
     pag.press('end') # 스크롤 최하단 이동
     pag.press('pgdn') # 조회
